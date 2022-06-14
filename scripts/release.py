8db62ec5cdefc8ee94c1c2fe38f4745a05147d76
Marco <marco.finotti89@gmail.com>
2022-06-14 17:45:49 +0200

    release fixes

--- scripts/release.py
+++ scripts/release.py
@@ -5,6 +5,6 @@ auth_data               = {}
 auth_data["username"]   = "mqtt_kairos"
 auth_data["password"]   = "kairos!"
 
-sw      = sys.argv[0]
-version = sys.argv[0]
+sw      = sys.argv[1]
+version = sys.argv[2]
 publish.single("kairostech/"+sw+"/version", version, hostname="localhost", port=1884, auth=auth_data, retain=True)
